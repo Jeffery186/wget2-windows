244417c63dde143738aad2cbd3547d1b21cf1cb1
ibook86 <zyh15637540388@gmail.com>
2021-09-30 20:10:49 +0800

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
+sudo apt-get install -y -qq autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
  libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
  libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint
 
@@ -11,5 +11,5 @@ sudo apt-get install -y autotools-dev rsync tar texinfo pkg-config doxygen pando
 wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
 tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
 
-sudo apt-get install -y mingw-w64 mingw-w64-i686-dev mingw-w64-tools
-sudo apt-get install -y make m4 automake
+sudo apt-get install -y -qq mingw-w64 mingw-w64-i686-dev mingw-w64-tools
+sudo apt-get install -y -qq make m4 automake
