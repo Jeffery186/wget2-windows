a1b4a083202c041d3295a62b45ab2d841c588110
ibook86 <zyh15637540388@gmail.com>
2021-09-30 20:06:55 +0800

    test

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 sudo apt-get update
 sudo apt-get install -y autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
  libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
- libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24
+ libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint
 
 # build libhsts
-git clone https://gitlab.com/rockdaboot/libhsts && cd libhsts && autoreconf -fi && ./configure && make && make check && cd ..
+#git clone https://gitlab.com/rockdaboot/libhsts && cd libhsts && autoreconf -fi && ./configure && make && make check && cd ..
 
 # build wget2
 wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
-tar -xzvf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
+tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
 
 sudo apt-get install -y mingw-w64 mingw-w64-i686-dev mingw-w64-tools
 sudo apt-get install -y make m4 automake
