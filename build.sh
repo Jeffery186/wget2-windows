85c668434ac25fb18aaf898c635b66f4c6045557
ibook86 <zyh15637540388@gmail.com>
2021-09-30 23:15:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 sudo apt-get -qq update
 sudo apt-get -y -qq install autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
- libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
- libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint libzstd-dev libpsl-dev libnghttp2-dev
+ libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 brotli libbrotli-dev libzstd1 libgnutls28-dev libidn2-0 flex libpsl5 libnghttp2-14 \
+ libmicrohttpd12 lzip clzip libgpgme-dev lcov libgpgme11 libpcre2-32-0 autopoint libzstd-dev libpsl-dev libnghttp2-dev
 
 # build libhsts
-#git clone https://gitlab.com/rockdaboot/libhsts && cd libhsts && autoreconf -fi && ./configure && make && make check && cd ..
+tar -xzf libhsts-master.tar.gz && cd libhsts-master && autoreconf -fi && ./configure && make && make check && make install && cd ..
 
 # build wget2
 wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
-tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure -h && ./configure && make && make check && sudo make install && cd ..
+tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
 
 sudo apt-get -y -qq install mingw-w64 mingw-w64-i686-dev mingw-w64-tools
 sudo apt-get -y -qq install make m4 automake
