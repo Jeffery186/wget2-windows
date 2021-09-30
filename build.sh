bd73bb28b6de3c46dd3f94548f2e03530d3bcfa4
ibook86 <zyh15637540388@gmail.com>
2021-10-01 01:10:03 +0800

    fix: cd

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir install
+export PWD_WORK_PATH=$PWD
 export INSTALL_PATH=$PWD/install
 sudo apt-get -qq update
 sudo apt-get -y -qq install tree wget git tar gcc cmake autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
@@ -7,18 +8,17 @@ sudo apt-get -y -qq install tree wget git tar gcc cmake autotools-dev rsync tar 
  libmicrohttpd-dev lzip clzip libgpgme-dev lcov libgpgme11 libpcre2-32-0 autopoint libzstd-dev libpsl-dev libnghttp2-dev
 
 # build libhsts
-wget https://gitlab.com/rockdaboot/libhsts/-/archive/master/libhsts-master.tar.gz && tar -xzf libhsts-master.tar.gz && cd libhsts-master && autoreconf -fi && ./configure && make && make check && sudo make install && cd ..
+wget https://gitlab.com/rockdaboot/libhsts/-/archive/master/libhsts-master.tar.gz && tar -xzf libhsts-master.tar.gz && cd libhsts-master && autoreconf -fi && ./configure && make && make check && sudo make install
 
 # build wget2
-wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
-tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
+cd cd $PWD_WORK_PATHwget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz && tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install
 
 sudo apt-get -y -qq install mingw-w64 mingw-w64-x86-64-dev mingw-w64-i686-dev mingw-w64-tools make m4 automake
 
-cd wget2-2.0.0
+cd $PWD_WORK_PATH/wget2-2.0.0
 ./configure \
  --host=x86_64-w64-mingw32 \
- --prefix=$INSTALL_PATH \
+ --prefix=$INSTALL_PATH
 make
 sudo make install
 mkdir $INSTALL_PATH/wget-gnutls
