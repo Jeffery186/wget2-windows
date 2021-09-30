340a21108344f75d451b99012864297ada637bfe
ibook86 <zyh15637540388@gmail.com>
2021-09-30 22:27:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update -qq
-sudo apt-get install -y -qq autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
+sudo apt-get -qq update
+sudo apt-get -y -qq install autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
  libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
  libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint
 
@@ -9,7 +9,7 @@ sudo apt-get install -y -qq autotools-dev rsync tar texinfo pkg-config doxygen p
 
 # build wget2
 wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
-tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
+tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure -h && ./configure && make && make check && sudo make install && cd ..
 
-sudo apt-get install -y -qq mingw-w64 mingw-w64-i686-dev mingw-w64-tools
-sudo apt-get install -y -qq make m4 automake
+sudo apt-get -y -qq install mingw-w64 mingw-w64-i686-dev mingw-w64-tools
+sudo apt-get -y -qq install make m4 automake
