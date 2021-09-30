0d4a88e9992b3865779fe43e6fd8b64710ed6835
ibook86 <zyh15637540388@gmail.com>
2021-09-30 22:34:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 sudo apt-get -qq update
 sudo apt-get -y -qq install autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
  libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
- libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint
+ libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint libzstd-dev libpsl-dev libnghttp2-dev
 
 # build libhsts
 #git clone https://gitlab.com/rockdaboot/libhsts && cd libhsts && autoreconf -fi && ./configure && make && make check && cd ..
