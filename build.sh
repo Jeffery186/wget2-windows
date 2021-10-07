45cb2a3e18ae684c6c831c7b1342aec1699e5c49
ibook86 <zyh15637540388@gmail.com>
2021-10-07 19:12:34 +0800

    update:build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y autotools-dev rsync tar texinfo pkg-config doxygen pandoc gettext libiconv-hook-dev \
- libiconv-hook1 zlib1g liblzma5 libbz2-1.0 libbrotli1 libzstd1 libgnutls30 libidn2-0 flex libpsl5 libnghttp2-14 \
- libmicrohttpd12 lzip lcov libgpgme11 libpcre2-32-0 libwolfssl24 autopoint
-
-# build libhsts
-#git clone https://gitlab.com/rockdaboot/libhsts && cd libhsts && autoreconf -fi && ./configure && make && make check && cd ..
-
-# build wget2
-wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz
-tar -xzf wget2-latest.tar.gz && cd wget2-2.0.0 && ./configure && make && make check && sudo make install && cd ..
-
-sudo apt-get install -y mingw-w64 mingw-w64-i686-dev mingw-w64-tools
-sudo apt-get install -y make m4 automake
+pwd
+tree
+wget https://gnuwget.gitlab.io/wget2/wget2-latest.tar.gz && tar -xf wget2-latest.tar.gz &&
+  cd wget2-2.0.0 &&
+  ./bootstrap
+cp pthread_sigmask.c.mingw lib/pthread_sigmask.c
+export GCCLIB=$(dirname $(find /usr/lib/gcc/$PREFIX -name libgcc_s_seh-1.dll | grep posix))
+export WINEPATH="$WINEPATH;/usr/$PREFIX/bin;/usr/$PREFIX/lib;$PWD/libwget/.libs;$GCCLIB"
+echo "WINEPATH=$WINEPATH"
+./configure \
+  --build=x86_64-pc-linux-gnu \
+  --host=$PREFIX \
+  --disable-shared &&
+  make -j$(nproc) &&
+  make install
